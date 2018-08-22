2c18e879830e777c016ee91f5b9ffae60b6b72ec
Adamouization <aj645@bath.ac.uk>
2018-08-22 16:38:08 +0100

    Create flask routing views test

---
+++ tests/test_views.py
@@ -0,0 +1,24 @@
+import unittest
+
+import app
+
+
+class IndexViewTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.app.test_client()
+        app.app.testing = True  # propagate the exceptions to the test client
+
+    def test_root_url(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.default_mimetype, 'text/html')
+
+    def test_index_url(self):
+        response = self.app.get('/index')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.default_mimetype, 'text/html')
+
+
+if __name__ == '__main__':
+    unittest.main()

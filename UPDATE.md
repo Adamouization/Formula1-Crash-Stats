ff869b22cda4a8ac812389bb60cad1c1ccc94001
Adam Jaamour <dev@jaamour.com>
2022-09-08 18:57:10 +0100

    Update unit tests

--- UPDATE.md
+++ UPDATE.md
@@ -1,5 +1,5 @@
 Upgrade pip
-`pip3 install --upgrade`
+`pip3 install --upgrade pip`
 
 Update all packages to latest
 `pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U`
--- tests/test_views.py
+++ tests/test_views.py
@@ -12,23 +12,26 @@ class IndexViewTestCase(unittest.TestCase):
     def test_root_url(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.default_mimetype, 'text/html')
+        self.assertEqual(response.mimetype, 'text/html')
+        self.assertEqual(response.charset, 'utf-8')
 
     def test_index_url(self):
         response = self.app.get('/index')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.default_mimetype, 'text/html')
+        self.assertEqual(response.mimetype, 'text/html')
+        self.assertEqual(response.charset, 'utf-8')
 
     def test_help_url(self):
         response = self.app.get('/help')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.default_mimetype, 'text/html')
-
+        self.assertEqual(response.mimetype, 'text/html')
+        self.assertEqual(response.charset, 'utf-8')
 
     def test_about_url(self):
         response = self.app.get('/about')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.default_mimetype, 'text/html')
+        self.assertEqual(response.mimetype, 'text/html')
+        self.assertEqual(response.charset, 'utf-8')
 
 
 if __name__ == '__main__':
